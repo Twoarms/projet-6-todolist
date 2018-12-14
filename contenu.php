682923fc54d2a4bb112e1287be5089336e3d8373
Twoarms <debras.mathieu@gmail.com>
2018-12-14 14:38:23 +0100

    change task status php done

--- contenu.php
+++ contenu.php
@@ -3,34 +3,62 @@
 $file = 'todo.json';
 $json = file_get_contents($file);
 $content = json_decode($json, true);
+$changed = [];
+if (isset($_POST["save"])) {
+    if (isset($_POST["todo"])) {
+        foreach($_POST["todo"] as $value) { // Pour chaque valeur des checkbox de nom todo
+            foreach($content as $key => $task) { // Pour chaque objet du json en tant que tâche
+                if ($task["id"] == $value) { // Si l'id de la tâche correspond a la valeur de checkbox
+                    $content[$key]["status"] = 1; // Statut de la tache = 1 aka done
+                    /* array_push($changed, $value); */ // Array avec id des objets dont statut a changé
+                } /* elseif ($task["id"] != $value && !in_array($task["id"], $changed)) { // Si l'id correspond pas et ne fait pas partie des objets modifiés cette fois
+                    $task["status"] = 0; // statut tache = 0 aka à faire
+                } */
+            }
+        }
+        $json = json_encode($content);
+        file_put_contents($file, $json);
+    } /* elseif (!isset($_POST["todo"])) { // Si aucune checkbox checked
+        foreach ($content as &$task) { // Pour chaque tâche
+            $task["status"] = 0; // Statut tache = 0
+        }
+    } */
+}
+
 ?>
 
     <div class="toDoList">
         
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
 
             <fieldset>
                 <legend>Tout doux :</legend>
+                <ul>
                 <?php
                 foreach($content as $task) {
                     if ($task["status"] == 0) {
-                        echo '<label><input type="checkbox">'.$task["task"].'</label>';
+                        echo '<li><label><input type="checkbox" name="todo[]" value="'.$task["id"].'" id="'.$task["id"].'">&nbsp;'.$task["task"].'</label></li>';
                     }
                 };
                 ?>
+                </ul>
             </fieldset>
+
 
             <fieldset>
                 <legend>Art cave :</legend>
+                <ul>
                 <?php
                 foreach($content as $task) {
                     if ($task["status"] == 1) {
-                        echo '<label><input type="checkbox">'.$task["task"].'</label>';
+                        echo '<li><label><input type="checkbox" checked name="" value="'.$task["id"].'" id="'.$task["id"].'">&nbsp;<del>'.$task["task"].'</del></label></li>';
                     }
                 }
                 ?>
+                </ul>
             </fieldset>
 
+                <button type="submit" name="save">Enregistrer</button>
         </form>
 
     </div>
--- formulaire.php
+++ formulaire.php
@@ -1,22 +1,28 @@
 <?php
 // Self-explanatory
 function sanitize($data) {
+    $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     $data = filter_var($data, FILTER_SANITIZE_STRING);
     return $data;
 }
 
-$newTask = trim($_POST["task"]); // => Trim not necessary in sanitize function in this case
+/* $newTask = trim($_POST["task"]); */
 $err = "";
-if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($newTask)) { /* If page is called with post (means form was submitted)
-                                                                    and trimmed input isn't empty */
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"]) && !empty(trim($_POST["task"]))/*  && empty($_POST["save"]) */) { /* If form is submitted,
+trimmed input isn't empty and page was not reloaded by "save" submit */
     $newTask = sanitize($_POST["task"]); // Sanitizing form input
     $file = 'todo.json';
     $json = file_get_contents($file); // Self-explanatory
     $content = json_decode($json, true); // Decodes json into php array
-    $task = ["task" => $newTask, "status" => 0]; // Prepares new object
+    $task = ["task" => $newTask, "status" => 0, "id" => 'whatevIllGetReplaced']; // Prepares new object
     array_push($content, $task); // Puts new object into php array decoded from json
+    $id = 0;
+    foreach($content as &$object) {
+        $object["id"] = $id;
+        $id++;
+    }
     $json = json_encode($content); // Encodes php array into json
     file_put_contents($file, $json); // writes file with latest json
 } else {
@@ -45,7 +51,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($newTask)) { /* If page is ca
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
             <label for="newTask">Nouvelle tâche :</label>
             <input class="input" type="text" id="newTask" name="task" <?php echo $err;?> required>
-            <button type="submit">Ajouter</button>
+            <button type="submit" name="submit">Ajouter</button>
         </form>
     </div>
 
